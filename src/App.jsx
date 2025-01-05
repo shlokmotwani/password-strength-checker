2ced813bd1b111c8337b41372693f4815fd0599b
Shlok Motwani <shlok.codes@gmail.com>
2025-01-05 16:24:02 +0530

    feat: pwd strength checker with color indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,48 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [category, setCategory] = useState({});
+
+  function handlePwdChange(e) {
+    setPassword(e.target.value);
+    categorizePwd();
+  }
+
+  function categorizePwd(){
+    if(password.length > 12){
+      setCategory({
+        name: "Strong",
+        color: "darkgreen"
+      });
+    }
+    else  if(password.length > 8){
+      setCategory({
+        name: "Medium",
+        color: "orange"
+      });
+    }
+    else{
+      setCategory({
+        name: "Weak",
+        color: "brown"
+      });
+    }
+  }
+
+  return (
+    <div id="main">
+      <div>
+        <h1>Password Strength Checker</h1>
+        <input type="text" value={password} onChange={handlePwdChange} />
+      </div>
+      <div id="result">
+      <span>{`Password strength : ${category.name}`}</span>
+      <div id="strength-indicator" style={{backgroundColor: category.color}}></div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
